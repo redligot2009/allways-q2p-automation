e9132807ce47e15f042dc3a574da852bd1e0fcd6
red.ligot <redligot2009.forreal@gmail.com>
2021-03-06 09:25:38 +0800

    Added fields = all to serializers, and specific readonly fields for Quotation.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -36,6 +36,13 @@ class JobOrderSerializer(serializers.ModelSerializer):
         model = JobOrder
         # fields = ('STATUS', 'joborder_number','production_status','joborder_quotation','joborder_number')
 
+""""
+========================================
+==== PRODUCTION RELATED SERIALIZERS ====
+========================================
+"""
+
+
 class PaperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paper
@@ -44,40 +51,62 @@ class PaperSerializer(serializers.ModelSerializer):
 class PrintingProcessSerializer(serializers.ModelSerializer):
     class Meta:
         model = PrintingProcess
+        fields=('__all__')
         # fields = ('process_id','process_name','process_base_factor')
 
 class LaminationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lamination
+        fields=('__all__')
         # fields = ('lamination_id','lamination_type','lamination_base_price')
 
 class DieCutSerializer(serializers.ModelSerializer):
     class Meta:
         model = DieCut
+        fields=('__all__')
         # fields = ('diecut_id','diecut_type','diecut_base_price')
 
 class BindingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Binding
+        fields=('__all__')
         # fields = ('binding_id','binding_type','binding_base_price')
 
 class ProductionConstantsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductionConstants
+        fields=('__all__')
         # fields = ('constants_id','plate_base_price','base_price_fold','lamination_factor','min_rate_running')
 
 class PlateSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Plate
+        fields=('__all__')
         # fields = ('plate_id','no_impressions','extra_impressions', 'total_impressions','running_costs')
 
 class QuotationItemSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = QuotationItem
-        # fields = ('COLOR','PROCESS','LAMINATED','FOLD','SPREAD','ITEM','q_item_id','q_number','color_specs_id','process_specs_id','lamination_specs_id','diecut_specs_id','folded_size_specs_id','spread_size_specs_id','binding_specs_id','item_type','item_type_other')
+        fields=('__all__')
 
 class QuotationSerializer(serializers.ModelSerializer):
+    total_no_plates = serializers.ReadOnlyField()
+    total_plate_costs = serializers.ReadOnlyField()
+    exact_no_sheets = serializers.ReadOnlyField()
+    extra_sheets = serializers.ReadOnlyField()
+    total_no_sheets = serializers.ReadOnlyField()
+    total_paper_costs = serializers.ReadOnlyField()
+    total_running_costs = serializers.ReadOnlyField()
+    total_signatures = serializers.ReadOnlyField()
+    total_folding_costs = serializers.ReadOnlyField()
+    total_gathering_costs = serializers.ReadOnlyField()
+    total_lamination_costs = serializers.ReadOnlyField()
+    raw_total_costs = serializers.ReadOnlyField()
+    final_unit_costs = serializers.ReadOnlyField()
+    final_total_costs = serializers.ReadOnlyField()
     class Meta:
         model = Quotation
-        # fields = ('STATUS','q_number','approval_status','q_quantity','product_specifications','q_product_number','q_am_employee_number','q_email','q_date','final_unit_costs','final_total_costs','total_folds','raw_total_costs','margin_of_error','markup_percentage','no_plates','pages_can_fit','project_file_path','project_dimensions_length','project_dimensions_width','no_colors')
+        fields=('__all__')
 
