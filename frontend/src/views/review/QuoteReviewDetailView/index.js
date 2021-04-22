9668ec861beb53ce4bb4d88a066016197fa97445
redligot2009 <redligot2009.forreal@gmail.com>
2021-04-22 09:18:06 +0800

    Initial setup for Quote Review page

---
+++ frontend/src/views/review/QuoteReviewDetailView/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormHelperText,
+  Link,
+  TextField,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import Page from 'src/components/Page';
+
+import { useDispatch, useSelector } from "react-redux";
+
+// import { register } from "../../_actions/auth";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      backgroundColor: theme.palette.background.dark,
+      height: '100%',
+      paddingBottom: theme.spacing(3),
+      paddingTop: theme.spacing(3)
+    }
+  }));
+
+const QuoteReviewDetailView = () => {
+    const classes = useStyles();
+    const navigate = useNavigate();
+    // const { message } = useSelector(state => state.message);
+    const dispatch = useDispatch();
+    return (
+        <Page
+          className={classes.root}
+          title="Register"
+        >
+            <Box
+                display="flex"
+                flexDirection="column"
+                overflow="auto"
+                height="100%"
+            >
+                <Container 
+                    maxWidth="sm"
+                >
+                    <Formik
+                        initialValues={{
+                            // TODO: Fetch quotation data and store in initialValues quotation object.
+                            quotation: {}
+                        }}
+                        onSubmit={(values, actions) => {
+
+                        }}
+                    >
+                    <Typography
+                        color="textPrimary"
+                        variant="h2"
+                    >
+                        Review and compute quotation
+                    </Typography>
+                    </Formik>
+                </Container>
+            </Box>
+        </Page>);
+}
+
+export default QuoteReviewDetailView;
