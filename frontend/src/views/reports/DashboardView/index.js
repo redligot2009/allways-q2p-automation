0633151280d6339b65f3da265874d6d535c71e35
redligot2009 <redligot2009.forreal@gmail.com>
2021-04-25 20:29:43 +0800

    Refactor 2:  Fixed layout issues, fetched quotation data via Redux actions.

--- frontend/src/views/reports/DashboardView/index.js
+++ frontend/src/views/reports/DashboardView/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link as RouterLink, useNavigate, useHistory, Link } from 'react-router-dom';
 
 import {
@@ -22,7 +22,7 @@ import data from  './data';
 import QuotationCardComputed from './QuotationCardComputed';
 import ProductCard from './ProductCard';
 
-
+import {getComputedQuotations, getInProgressQuotations} from '../../../_actions/quotation';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,138 +41,166 @@ const Dashboard = () => {
   const { profile : currentUserProfile } = useSelector(state => state.auth);
   
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  
+  const {computedQuotations : computed} = useSelector(state => state.quotation);
+  const {inProgressQuotations: in_progress} = useSelector(state=>state.quotation);
 
-  return (
+  useEffect(()=>{
+    async function fetchData() {
+      await dispatch(getComputedQuotations())
+      await dispatch(getInProgressQuotations())
+    }
+    fetchData();
+  },[])
+
+  return (currentUserProfile && 
     <Page
       className={classes.root}
       title="Dashboard"
     >
-
       <Container maxWidth={false}>
-      <Box mb={2}>
-      <Typography className={classes.name}
-      color="textPrimary"
-      variant="h2"
-      >
-        Hello {currentUserProfile.full_name}! What would you like to do today?
-      </Typography>
-      </Box>
-        <Grid
-          container
-          spacing={3}
-          direction="row"
-          justify="flex-start"
-          alignItems="flex-start"
-        >
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <Link to={'/app/quote/review/'}>
-              <Budget />
-            </Link>
-          </Grid>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <Link to={'/'}>
-              <TotalCustomers />
-            </Link>
-          </Grid>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <Link to={'/app/employees/'}>
-              <TasksProgress />
-            </Link>
-          </Grid>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <Link to={'/app/settings'}>
-              <TotalProfit />
-            </Link>
-          </Grid>
-          <Box ml={1} width={1} height={1}>
-          <Typography className={classes.name}
-          color="textPrimary"
-          variant="h2"
+        <Box mb={2}>
+          <Typography 
+            className={classes.name}
+            color="textPrimary"
+            variant="h2"
           >
-          Overview of Orders
+            Hello {currentUserProfile.full_name}! What would you like to do today?
           </Typography>
           </Box>
-          <Grid
-            item
-            
-          >
-          <Typography className={classes.name}
-          color="textSecondary"
-          variant="h5"
-          >
-          AWAITING APPROVAL
-          </Typography>
-          {data.in_progress.map((quotation) => (
-                  <Grid
-                    item
-                    key={quotation.id}
-                    lg={12}
-                    md={6}
-                    xs={12}
+            <Grid
+              container
+              spacing={3}
+              direction="row"
+              justify="flex-start"
+              alignItems="flex-start"
+            >
+              <Grid item lg={3} sm={6} xl={3} xs={12}>
+                <Link to={'/app/quote/review/'}>
+                  <Budget />
+                </Link>
+              </Grid>
+              <Grid item lg={3} sm={6} xl={3} xs={12}>
+                <Link to={'/app/tracking/account_manager'}>
+                  <TotalCustomers />
+                </Link>
+              </Grid>
+              <Grid item lg={3} sm={6} xl={3} xs={12}>
+                <Link to={'/app/employees/'}>
+                  <TasksProgress />
+                </Link>
+              </Grid>
+              <Grid item lg={3} sm={6} xl={3} xs={12}>
+                <Link to={'/app/settings'}>
+                  <TotalProfit />
+                </Link>
+              </Grid>
+              <Box ml={1} width={1} height={1}>
+                <Box my={2}>
+                  <Typography 
+                    className={classes.name}
+                    color="textPrimary"
+                    variant="h2"
+                    mb={3}
                   >
-                    <Box mt={2}>
-                    <QuotationCard
-                      className={classes.QuotationCard}
-                      quotation={quotation}
-                    />
+                    Overview of Quotes
+                  </Typography>
+                </Box>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={6}>
+                    <Box mb={1}>
+                      <Typography className={classes.name}
+                        color="textSecondary"
+                        variant="h5"
+                      >
+                        AWAITING COMPUTATION
+                      </Typography>
                     </Box>
-                  </Grid>
-          ))}
-          </Grid>
-          <Grid
-            item
-
-          >
-          <Typography className={classes.name}
-          color="textSecondary"
-          variant="h5"
-          >
-          IN PRODUCTION
-          </Typography>
-          {/* {products.map((product) => (
-                  <Grid
-                    item
-                    key={product.id}
-                    lg={12}
-                    md={6}
-                    xs={12}
-                  >
-                    <Box mt={2}>
-                    <ProductCard
-                      className={classes.productCard}
-                      product={product}
-                    />
-                    </Box>
-                  </Grid>
-                ))} */}
-          </Grid>
-          <Grid
-            item
-            
-          >
-          <Typography className={classes.name}
-          color="textSecondary"
-          variant="h5"
-          >
-          OUT FOR DELIVERY
-          </Typography>
-          {data.computed.map((quotation) => (
-                    <Grid
-                      item
-                      key={quotation.id}
-                      lg={12}
-                      md={6}
-                      xs={12}
-                    >
-                      <Box mt={2}>
-                      <QuotationCardComputed
-                        className={classes.QuotationCard}
-                        quotation={quotation}
-                      />
-                      </Box>
+                    <Grid container style={{maxHeight:480,overflow:'auto'}}>
+                      {in_progress && in_progress.map((quotation) => (
+                        <Grid
+                          item
+                          key={quotation.id}
+                        >
+                          <Box mt={2}>
+                          <QuotationCard
+                            className={classes.QuotationCard}
+                            quotation={quotation}
+                          />
+                          </Box>
+                        </Grid>
+                    ))}
                     </Grid>
-                  ))}
-          </Grid>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <Box mb={1}>
+                      <Typography className={classes.name}
+                        color="textSecondary"
+                        variant="h5"
+                      >
+                        AWAITING APPROVAL
+                      </Typography>
+                    </Box>
+                    <Grid container style={{maxHeight:480,overflow:'auto'}}>
+                      {computed && computed.map((quotation) => (
+                        <Grid
+                          item
+                          key={quotation.id}
+                        >
+                          <Box mt={2}>
+                          <QuotationCard
+                            className={classes.QuotationCard}
+                            quotation={quotation}
+                          />
+                          </Box>
+                        </Grid>
+                    ))}
+                  </Grid>
+                </Grid>
+              </Grid>
+              
+              <Box ml={1} width={1} height={1}>
+                <Box my={2}>
+                  <Typography 
+                    className={classes.name}
+                    color="textPrimary"
+                    variant="h2"
+                    mb={3}
+                  >
+                    Overview of Job Orders
+                  </Typography>
+                </Box>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={6}>
+                    <Box mb={1}>
+                      <Typography className={classes.name}
+                        color="textSecondary"
+                        variant="h5"
+                      >
+                        IN PRODUCTION
+                      </Typography>
+                    </Box>
+                    <Grid container style={{maxHeight:480,overflow:'auto'}}>
+                      
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <Box mb={1}>
+                      <Typography className={classes.name}
+                        color="textSecondary"
+                        variant="h5"
+                      >
+                        OUT FOR DELIVERY
+                      </Typography>
+                    </Box>
+                    <Grid container style={{maxHeight:480,overflow:'auto'}}>
+                      
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
         </Grid>
       </Container>
     </Page>
