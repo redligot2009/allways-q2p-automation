c42035fc0209286596e8801b8ca8bb7537b617a0
redligot2009 <redligot2009.forreal@gmail.com>
2021-04-26 08:07:40 +0800

    Added more fields to AddEmployeeDialog and a submit button

---
+++ frontend/src/views/employee/EmployeeListView/AddEmployeeDialog.js
@@ -0,0 +1,241 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import {
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+  Button,
+  Avatar,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  makeStyles,
+  TextField
+} from '@material-ui/core';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import { AddBoxOutlined } from '@material-ui/icons';
+import { getJobPosition } from "../../../_helpers"
+
+import { Link as RouterLink, useNavigate, useLocation } from 'react-router-dom';
+import * as Yup from 'yup';
+import { Formik, Form, FieldArray, getIn } from 'formik';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  statsItem: {
+    alignItems: 'center',
+    display: 'flex'
+  },
+  statsIcon: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const AddEmployeeDialog = (props) => {
+  const classes = useStyles();
+  return (
+    <Dialog open={props.openAddEmployeeDialog} onClose={props.handleCloseAddEmployeeDialog} fullWidth={true} maxWidth = {'sm'}>
+      {/* <DialogTitle>HELLO WORLD</DialogTitle> */}
+      <DialogContent>
+        <Formik
+          enableReinitialize={true}
+          initialValues={{
+              "username": "",
+              "email": "placeholder.email@gmail.com",
+              "first_name": "",
+              "middle_name": "",
+              "last_name": "",
+              "mobile_number": "",
+              "shipping_address": "",
+              "organization_name": "",
+              "job_position": "P",
+              "plate_number": "",
+              "license_number": "",
+          }}
+          validationSchema={
+            Yup.object().shape({
+              username: Yup.string().required('Username is required'),
+              email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+              mobile_number: Yup.string().required().min(10, "Must be a valid phone number"),
+              first_name: Yup.string().max(255).required('First name is required'),
+              middle_name: Yup.string().max(255),
+              last_name: Yup.string().max(255).required('Last name is required'),
+              // TODO: Mimic Django password validation behavior
+              password: Yup.string()
+                        .max(255)
+                        .required('Password is required')
+                        .matches(
+                          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                          `Must contain: 8 characters, 1 uppercase, 1 lowercase, 1 number, and 1 special character.`
+                        )
+            })
+          }
+        >
+          {({
+            errors,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+            touched,
+            values
+          }) => (
+              <form 
+                onSubmit={handleSubmit}
+              >
+                <Typography
+                  className={classes.name}
+                  color="textSecondary"
+                  variant="h3"
+                >
+                  Add {getJobPosition(props.employeeType)} Account
+                </Typography>
+                <Typography
+                  className={classes.name}
+                  color="textSecondary"
+                  variant="h5"
+                >
+                  Account Credentials
+                </Typography>
+                <TextField
+                  error={Boolean(touched.username && errors.username)}
+                  fullWidth
+                  helperText={touched.username && errors.username}
+                  label="Username"
+                  margin="normal"
+                  name="username"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.username}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.password && errors.password)}
+                  fullWidth
+                  helperText={touched.password && errors.password}
+                  label="Password"
+                  margin="normal"
+                  name="password"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="password"
+                  value={values.password}
+                  variant="outlined"
+                />
+                <Typography
+                  className={classes.name}
+                  color="textSecondary"
+                  variant="h5"
+                >
+                  Employee Basic Information
+                </Typography>
+                <TextField
+                  error={Boolean(touched.first_name && errors.first_name)}
+                  fullWidth
+                  helperText={touched.first_name && errors.first_name}
+                  label="First name"
+                  margin="normal"
+                  name="first_name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.first_name}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.middle_name && errors.middle_name)}
+                  fullWidth
+                  helperText={touched.middle_name && errors.middle_name}
+                  label="Middle name"
+                  margin="normal"
+                  name="middle_name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.middle_name}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.last_name && errors.last_name)}
+                  fullWidth
+                  helperText={touched.last_name && errors.last_name}
+                  label="Last name"
+                  margin="normal"
+                  name="last_name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.last_name}
+                  variant="outlined"
+                />
+                <Typography
+                  className={classes.name}
+                  color="textSecondary"
+                  variant="h5"
+                >
+                  Employee Contact Information
+                </Typography>
+                <TextField
+                  error={Boolean(touched.email && errors.email)}
+                  fullWidth
+                  helperText={touched.email && errors.email}
+                  label="Email Address"
+                  margin="normal"
+                  name="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="email"
+                  value={values.email}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.mobile_number && errors.mobile_number)}
+                  fullWidth
+                  helperText={touched.mobile_number && errors.mobile_number}
+                  label="Mobile Number"
+                  margin="normal"
+                  name="mobile_number"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.mobile_number}
+                  variant="outlined"
+                />
+              {()=>{
+                switch(props.employeeType)
+                {
+                  case 'O':
+                    return <></>
+                  case 'AM':
+                    return <></>
+                  case 'P':
+                    return <></>
+                  case 'D':
+                    return <></>
+                  default:
+                    return <></>
+                }
+              }}
+              <Button
+                  color="primary"
+                  // disabled={isSubmitting}
+                  fullWidth
+                  size="large"
+                  type="button"
+                  variant="outlined"
+                  onClick={handleSubmit}
+                  // onClick={handleSubmit}
+              >
+                  Create Account
+              </Button>
+            </form>
+          )}
+        </Formik>
+      </DialogContent>
+    </Dialog>
+  )
+}
+export default AddEmployeeDialog;
--- frontend/src/views/employee/EmployeeListView/index.js
+++ frontend/src/views/employee/EmployeeListView/index.js
@@ -11,6 +11,7 @@ import {
 } from '@material-ui/core';
 import Page from 'src/components/Page';
 import EmployeeCard from './EmployeeCard';
+import AddEmployeeDialog from './AddEmployeeDialog';
 import data from './data';
 import {
   getAccountManagerEmployees,
@@ -38,7 +39,6 @@ const EmployeeList = () => {
   const {owners} = useSelector(state=>state.users);
   const {productionEmployees} = useSelector(state=>state.users);
   const {driverEmployees} = useSelector(state=>state.users);
-  // const [employee] = useState(data);
 
   useEffect(()=>{
     async function fetchData () {
@@ -57,12 +57,22 @@ const EmployeeList = () => {
     fetchData();
   },[]);
 
+  const [openAddEmployeeDialog, setOpenAddEmployeeDialog] = useState(true);
+  const [employeeType, setEmployeeType] = useState('O');
+
   return (
     <Page
       className={classes.root}
       title="Order Tracking"
     >
       <Container maxWidth={false}>
+        <AddEmployeeDialog
+          employeeType={employeeType}
+          openAddEmployeeDialog={openAddEmployeeDialog}
+          handleCloseAddEmployeeDialog={()=>{
+            setOpenAddEmployeeDialog(false);
+          }}
+        />
         <Typography
           className={classes.name}
           color="textPrimary"
@@ -71,79 +81,79 @@ const EmployeeList = () => {
           Employee Authorization
         </Typography>
         <Box mt={2}>
-        <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={4}>
-            <Typography
-              className={classes.name}
-              color="textSecondary"
-              variant="h5"
-            >
-              Account Manager/Owner Access
-            </Typography>
-              <Box mt={2}>
-                {accountManagers && accountManagers.map((employee) => (
-                  <Grid
-                    item
-                    key={employee.id}
-                    xs={12}
-                  >
-                    <Box mt={2}>
-                    <EmployeeCard
-                      className={classes.employeeCard}
-                      employee={employee}
-                    />
-                    </Box>
-                  </Grid>
-                ))}
-                {owners && owners.map((employee) => (
-                  <Grid
-                    item
-                    key={employee.id}
-                    xs={12}
-                  >
-                    <Box mt={2}>
-                    <EmployeeCard
-                      className={classes.employeeCard}
-                      employee={employee}
-                    />
-                    </Box>
-                  </Grid>
-                ))}
-              </Box>
-            </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Typography
-              className={classes.name}
-              color="textSecondary"
-              variant="h5"
-            >
-              Production Team Access
-            </Typography>
-              <Box mt={2}>
-                {productionEmployees && productionEmployees.map((employee) => (
-                  <Grid
-                    item
-                    key={employee.id}
-                    xs={12}
-                  >
-                    <Box mt={2}>
-                    <EmployeeCard
-                      className={classes.employeeCard}
-                      employee={employee}
-                    />
-                    </Box>
-                  </Grid>
-                ))}
-              </Box>
-            </Grid>
+          <Grid container spacing={3}>
             <Grid item xs={12} sm={6} md={4}>
               <Typography
                 className={classes.name}
                 color="textSecondary"
                 variant="h5"
               >
-                Driver Access
+                Account Manager/Owner Access
               </Typography>
+                <Box mt={2}>
+                  {accountManagers && accountManagers.map((employee) => (
+                    <Grid
+                      item
+                      key={employee.id}
+                      xs={12}
+                    >
+                      <Box mt={2}>
+                        <EmployeeCard
+                          className={classes.employeeCard}
+                          employee={employee}
+                        />
+                      </Box>
+                    </Grid>
+                  ))}
+                  {owners && owners.map((employee) => (
+                    <Grid
+                      item
+                      key={employee.id}
+                      xs={12}
+                    >
+                      <Box mt={2}>
+                        <EmployeeCard
+                          className={classes.employeeCard}
+                          employee={employee}
+                        />
+                      </Box>
+                    </Grid>
+                  ))}
+                </Box>
+              </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Typography
+                className={classes.name}
+                color="textSecondary"
+                variant="h5"
+              >
+                Production Team Access
+              </Typography>
+                <Box mt={2}>
+                  {productionEmployees && productionEmployees.map((employee) => (
+                    <Grid
+                      item
+                      key={employee.id}
+                      xs={12}
+                    >
+                      <Box mt={2}>
+                        <EmployeeCard
+                          className={classes.employeeCard}
+                          employee={employee}
+                        />
+                        </Box>
+                    </Grid>
+                  ))}
+                </Box>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Typography
+                  className={classes.name}
+                  color="textSecondary"
+                  variant="h5"
+                >
+                  Driver Access
+                </Typography>
                 <Box mt={2}>
                   {driverEmployees && driverEmployees.map((employee) => (
                     <Grid
@@ -152,17 +162,17 @@ const EmployeeList = () => {
                       xs={12}
                     >
                       <Box mt={2}>
-                      <EmployeeCard
-                        className={classes.employeeCard}
-                        employee={employee}
-                      />
+                        <EmployeeCard
+                          className={classes.employeeCard}
+                          employee={employee}
+                        />
                       </Box>
                     </Grid>
                   ))}
                 </Box>
               </Grid>
             </Grid>
-          </Box>
+        </Box>
       </Container>
     </Page>
   );
