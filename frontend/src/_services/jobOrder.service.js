43c6a18932a19354f2f55e8ffd67343ebb9510a7
redligot2009 <redligot2009.forreal@gmail.com>
2021-04-26 12:21:44 +0800

    Initial implementation of Job Order service API functions

--- frontend/src/_services/jobOrder.service.js
+++ frontend/src/_services/jobOrder.service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {getResultURL, getFilteredObject} from "../_helpers/"
 
 /*
 ///////////////////////////////
@@ -6,22 +7,36 @@ JOB ORDER API SERVICE FUNCTIONS
 ///////////////////////////////
 */
 
-const createJobOrder = (quotation) => {
+const createJobOrder = (jobOrder) => {
   // TODO: Implement API call to create new job order from a given quotation.
+  const createResult = axios.post(`api/joborders/`,jobOrder)
+  return createResult;
 }
 
 const retrieveJobOrder = (id) => {
   // TODO: Implement API call to retrieve job order details by ID
+  const jobOrderResult = axios.get(`api/joborders/${id}`)
+  return jobOrderResult;
 }
 
-const retrieveJobOrders = () => {
+const retrieveJobOrders = (production_status, manager, client) => {
   // TODO: Implement API call to retrieve list of job orders
+  const urlParams = {
+    "production_status" : production_status,
+    "manager" : manager,
+    "quotation__client__user" : client,
+  }
+  let requestURL = getResultURL("api/joborders/", urlParams);
+  const quotationsListResult = axios.get(requestURL);
+  return quotationsListResult;
 }
 
 const updateJobOrder = (jobOrder) => {
   // TODO: Implement API call to update a specific job order
+  const updateResult = axios.put(`api/quotations/${jobOrder.id}/`,jobOrder)
+  return updateResult;
 }
 
-const deleteJobOrder = (jobOrder) => {
+const deleteJobOrder = (id) => {
   // TODO: Implement API call to delete a specific job order
 }
