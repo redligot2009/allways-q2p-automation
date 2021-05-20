a113e1c14e2a90cd5d6fb406252f02f397f4d299
Ian Red Ligot <redligot2009.forreal@gmail.com>
2021-05-20 22:18:35 +0800

    Fixed bug with reload of screen not redirecting to correct page.

--- api/views.py
+++ api/views.py
@@ -24,16 +24,16 @@ import logging
 
 # SERVING REACT FRONTEND
 
-# class Assets(View):
+class Assets(View):
 
-#     def get(self, _request, filename):
-#         path = os.path.join(os.path.dirname(__file__), 'static', filename)
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
 
-#         if os.path.isfile(path):
-#             with open(path, 'rb') as file:
-#                 return HttpResponse(file.read(), content_type='application/javascript')
-#         else:
-#             return HttpResponseNotFound()
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
 
 #######################################
 ### USER / ACCOUNT RELATED VIEWSETS ###
--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
 ]
