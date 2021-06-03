e14c96cef247707e1b4e999f052990fc26e7c4b4
Ian Red Ligot <redligot2009.forreal@gmail.com>
2021-06-04 01:08:17 +0800

    Remove notification bell

--- src/layouts/DashboardLayout/TopBar.js
+++ src/layouts/DashboardLayout/TopBar.js
@@ -45,15 +45,6 @@ const TopBar = ({
         <Box flexGrow={1} />
         <Hidden mdDown>
           <IconButton color="inherit">
-            <Badge
-              badgeContent={notifications.length}
-              color="primary"
-              variant="dot"
-            >
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <IconButton color="inherit">
             <InputIcon />
           </IconButton>
         </Hidden>
